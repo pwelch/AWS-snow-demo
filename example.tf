4b5c7db3108a77bda47784b8ebeb8835376e7302
Paul Welch <git@welch.pw>
2020-01-08 14:16:41 -0500

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,7 +1,8 @@
 provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-00bf61217e296b409"
+  ami           = "	ami-00a208c7cdba991ea"
   instance_type = "t2.micro"
-  }
+}
