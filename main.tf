fb927934e406bf12ed3c83515b031ce382c490fc
pwelch <pwelch@hashicorp.com>
2020-01-08 15:00:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,17 +11,20 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-00a208c7cdba991ea"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.us-east-1d-public.id
+  subnet_id     = "subnet-108f882f"
+#  subnet_id     = aws_subnet.us-east-1d-public.id
+
+  associate_public_ip_address = true
 }
 
-resource "aws_vpc" "example" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-resource "aws_subnet" "us-east-1d-public" {
-  vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.0.1.0/25"
-  availability_zone = "us-east-1d"
-}
+#resource "aws_vpc" "example" {
+#  cidr_block           = "10.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#}
+#
+#resource "aws_subnet" "us-east-1d-public" {
+#  vpc_id            = aws_vpc.example.id
+#  cidr_block        = "10.0.1.0/25"
+#  availability_zone = "us-east-1d"
+#}
